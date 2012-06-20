b2956c9a5ce726e6bf9c0a8d5cf3230ea373029f
Hsin-Yen Liu <hsinyen@ymail.com>
2012-06-20 08:51:07 -0400

    - Edited the code such that it can work if there are spaces in the response

--- parser0.py
+++ parser0.py
@@ -1,23 +1,22 @@
 # Created:     19/06/2012
 
 a='<xml><network><subdomain>SUBDOMAIN_NAME_HERE</subdomain><id>NETWORK_ID_HERE</id></network></xml>'
-
+a='<xml><inventory><date>DATE</date><zones><zone><id>ZONE_ID</id><name>ZONE_NAME</name><impressions>IMPRESSIONS_NUMBER_FOR_THE_ZONE</impressions></zone></zones><total_impressions>TOTAL_IMPRESSIONS_NUMBER</total_impressions></inventory></xml>'
 # start: start index of the dictionary key
 # end: end index of the dictionary value
 def parseResponse (code, root):
     res={};
-    key=root
-    start=code.find (key)+len(key)+2
+    start=code.find (root)+len(root)+2
     while(code[start]!='/'):
         end=code.find('>',start)
 
         key=code[start:end]
-        res[key]= code[start+1:code.find('</'+key)]
+        res[key]= code[end+1:code.find('</'+key)]
 
         start=code.find('</'+key)+len(key)+4
     return res
 
-print parseResponse(a, 'network')
+print parseResponse(a, 'inventory')
 
 
 
