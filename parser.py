7920edb70b6abc9db10cc92ccbdd8741d9da6235
Hsin-Yen Liu <hsinyen@ymail.com>
2012-06-21 11:10:55 -0400

    Now returns a list of dictionaries if the root tag appears more than once
    Signed-off-by: Hsin-Yen Liu <hsinyen@ymail.com>

--- parser.py
+++ parser.py
@@ -3,33 +3,44 @@
 a='<xml><ad><name>AD_NAME</name><id>AD_ID</id><country><name>COUNTRY_NAME</name><impressions>AD_IMPRESSIONS_NUMBER</impressions><clicks>AD_CLICKS_NUMBER</clicks><cost>AD_COSTS</cost></country></ad></xml>'
 b=' <xml><network><subdomain>SUBDOMAIN_NAME_HERE</subdomain><id>NETWORK_ID_HERE</id></network></xml>'
 a='<xml><campaign_stats><creative_id>CREATIVE_ID_HERE</creative_id><creative_name>CREATIVE_NAME_HERE</creative_name><campaign_id>CAMPAIGN_ID_HERE</campaign_id><advertiser_id>ADVERTISER_ID_HERE</advertiser_id><advertiser_name>ADVERTISER_NAME_HERE</advertiser_name><company_name>ADVERTISERS_COMPANY_NAME</company_name><campaign_name>CAMPAIGN_NAME_HERE</campaign_name><network_id>NETWORK_ID_HERE</network_id><cost>COST_VALUE_HERE</cost><impressions>AD_IMPRESSIONS_NUMBER</impressions><clicks>AD_CLICKS_NUMBER</clicks></campaign_stats></xml>'
-b='<xml><inventory><date>DATE</date><zones><zone><id>ZONE_ID</id><name>ZONE_NAME</name><impressions>IMPRESSIONS_NUMBER_FOR_THE_ZONE</impressions></zone></zones><total_impressions>TOTAL_IMPRESSIONS_NUMBER</total_impressions></inventory></xml>'
+a='<xml><inventory><date>DATE</date><zones><zone><id>ZONE_ID</id><name>ZONE_NAME</name><impressions>IMPRESSIONS_NUMBER_FOR_THE_ZONE</impressions></zone></zones><total_impressions>TOTAL_IMPRESSIONS_NUMBER</total_impressions></inventory></xml>'
+a='<xml><network><subdomain>SUBDOMAIN_NAME_HERE1</subdomain><id>NETWORK_ID_HERE1</id></network><network><subdomain>SUBDOMAIN_NAME_HERE3</subdomain><id>NETWORK_ID_HERE3</id></network><network><subdomain>SUBDOMAIN_NAME_HERE2</subdomain><id>NETWORK_ID_HERE2</id></network></xml>'
 # start: start index of the dictionary key
 # end: end index of the dictionary value
+
+
 def parseResponse (code, root):
 
-    if root=='!':  #if root is '!', it means that the dictionary is nested (see below)
-        if code.find('>')==-1: #value is a simple string; not nested
+    if root == '!':  #if root is '!', it means that the dictionary is nested (see below)
+        if code.find('>') == -1: #value is a simple string; not nested
             return code
         else: #value is nested
             start=1
     else:
-         start=code.find('<',code.find (root))+1
+         list = code.split ('</' + root + '>')
+         if (len(list) > 2):
+            dictList = []
+            for i in range (len(list) - 1):
+                dictList.append(parseResponse (list [i] , root))
 
-    res={};
+            return dictList
 
-    while(start!=0 and code[start]!='/'):
-        end=code.find('>',start)
+         start = code.find('<', code.find (root)) + 1
+
+    res = {};
+
+    while(start != 0 and code[start] != '/'):
+        end = code.find('>', start)
         #print start,end,code
         key=code[start:end]
 
         #pass the value into parseResponse in case that the value is nested
-        res[key]= parseResponse(code[end+1:code.find('</'+key+'>')], '!')
+        res[key] = parseResponse(code[end+1:code.find('</' + key + '>')], '!')
 
-        start=code.find('<',code.find('</'+key)+1)+1
+        start=code.find('<', code.find('</'+key) + 1) + 1
     return res
 
-print parseResponse(a, 'campaign_stats')
+print parseResponse(a, 'network')
 
 
 
