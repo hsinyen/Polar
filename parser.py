3bd8748e43b84476ea6026e97a0ea1ccdb1a6f27
Hsin-Yen Liu <hsinyen@ymail.com>
2012-07-29 17:31:43 -0400

    This is a new parser that uses lxml to help parse xml

--- parser.py
+++ parser.py
@@ -1,14 +1,16 @@
 # Created:     19/06/2012
-
+from lxml import etree
 a='<xml><ad><name>AD_NAME</name><id>AD_ID</id><country><name>COUNTRY_NAME</name><impressions>AD_IMPRESSIONS_NUMBER</impressions><clicks>AD_CLICKS_NUMBER</clicks><cost>AD_COSTS</cost></country></ad></xml>'
 b=' <xml><network><subdomain>SUBDOMAIN_NAME_HERE</subdomain><id>NETWORK_ID_HERE</id></network></xml>'
 a='<xml><campaign_stats><creative_id>CREATIVE_ID_HERE</creative_id><creative_name>CREATIVE_NAME_HERE</creative_name><campaign_id>CAMPAIGN_ID_HERE</campaign_id><advertiser_id>ADVERTISER_ID_HERE</advertiser_id><advertiser_name>ADVERTISER_NAME_HERE</advertiser_name><company_name>ADVERTISERS_COMPANY_NAME</company_name><campaign_name>CAMPAIGN_NAME_HERE</campaign_name><network_id>NETWORK_ID_HERE</network_id><cost>COST_VALUE_HERE</cost><impressions>AD_IMPRESSIONS_NUMBER</impressions><clicks>AD_CLICKS_NUMBER</clicks></campaign_stats></xml>'
 a='<xml><inventory><date>DATE</date><zones><zone><id>ZONE_ID</id><name>ZONE_NAME</name><impressions>IMPRESSIONS_NUMBER_FOR_THE_ZONE</impressions></zone></zones><total_impressions>TOTAL_IMPRESSIONS_NUMBER</total_impressions></inventory></xml>'
-a='<xml><network><subdomain>SUBDOMAIN_NAME_HERE1</subdomain><id>NETWORK_ID_HERE1</id></network><network><subdomain>SUBDOMAIN_NAME_HERE3</subdomain><id>NETWORK_ID_HERE3</id></network><network><subdomain>SUBDOMAIN_NAME_HERE2</subdomain><id>NETWORK_ID_HERE2</id></network></xml>'
+#a='<xml><network><subdomain>SUBDOMAIN_NAME_HERE1</subdomain><id>NETWORK_ID_HERE1</id></network><network><subdomain>SUBDOMAIN_NAME_HERE3</subdomain><id>NETWORK_ID_HERE3</id></network><network><subdomain>SUBDOMAIN_NAME_HERE2</subdomain><id>NETWORK_ID_HERE2</id></network></xml>'
 # start: start index of the dictionary key
 # end: end index of the dictionary value
 
 
+root= etree.fromstring(a)
+print root.tag
 def parseResponse(code, root):
 
     # If root is '!', it means that the dictionary is nested (see below):
@@ -42,7 +44,7 @@ def parseResponse(code, root):
         start = code.find('<', code.find('</' + key) + 1) + 1
     return res
 
-print parseResponse(a, 'network')
+print parseResponse(a, 'inventory')
 
 
 
---
+++ parserNew.py
@@ -0,0 +1,27 @@
+#Last Update: July 29th
+
+from lxml import etree
+
+a = '<xml><network><subdomain>SUBDOMAIN_NAME_HERE</subdomain><id>NETWORK_ID_HERE</id></network></xml>';
+a = '<xml><network><subdomain>SUBDOMAIN_NAME_HERE1</subdomain><id>NETWORK_ID_HERE1</id></network><network><subdomain>SUBDOMAIN_NAME_HERE3</subdomain><id>NETWORK_ID_HERE3</id></network><network><subdomain>SUBDOMAIN_NAME_HERE2</subdomain><id>NETWORK_ID_HERE2</id></network></xml>'
+a = '<xml><ad><name>AD_NAME</name><id>AD_ID</id><country><name>COUNTRY_NAME</name><impressions>AD_IMPRESSIONS_NUMBER</impressions><clicks>AD_CLICKS_NUMBER</clicks><cost>AD_COSTS</cost></country></ad></xml>'
+
+# This is a helper function for parseResponse(); it creates a dictionary for
+# each appearance of the root tag
+def parseRoot(child):
+    res = {}
+    for c in child:
+        res [c.tag] = parseRoot(c) if len(c) > 0 else c.text;
+    return res;
+
+# This function looks for the root tag(s) and returns a list of dictionaries,
+# one dictionary each root tag
+def parseResponse(code, rootTag):
+    root = etree.fromstring (code);
+    list = [parseRoot(element) for element in root.iter (rootTag)]
+
+    if (len (list) == 1):
+        return list[0];
+    return list;
+
+print parseResponse (a, 'ad');
